2e3cc0f34079c4721500d4ba1b521610ef97bfbe
rpeschke <peschke@hawaii.edu>
2019-10-28 18:39:32 +0900

    added a state machine to the counter. now the counter now that it was running

--- counter.py
+++ counter.py
@@ -1,6 +1,7 @@
 import argparse
 import os,sys,inspect
 import copy
+from enum import Enum 
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
@@ -11,42 +12,60 @@ from CodeGen.xgenPackage import *
 from CodeGen.xgenDB import *
 
 
+class counter_state(Enum):
+    idle    = 0
+    running = 1
+    done    = 2
+
 class counter(v_class):
     def __init__(self, CounterLength):
         super().__init__("counter_"+ str(CounterLength))
         self.__v_classType__         = v_classType_t.Slave_t
         
-        self.Running = v_sl()
+        self.state = v_enum(counter_state)
         self.Count = v_slv(CounterLength)
         self.MaxCount = v_slv(CounterLength)
 
     def _onPull(self):
-        if self.Running:
+        if self.isRunning():
             self.Count <<  self.Count +1
 
-        if self.Count >= self.MaxCount:
-            self.Running << 0
+        if self.isRunning() and self.Count >= self.MaxCount:
+            self.state << counter_state.done
             self.Count << 0
+
     
 
     def StartCountTo(self, MaxCount= port_in(v_slv())):
-        if not self.Running:
-            self.Running<<1
+        if self.isReady():
+            self.state << counter_state.running
             self.MaxCount << MaxCount
             self.Count << 0
+            
 
     def stopCounter(self):
-        self.Running << 0
+        self.state << counter_state.done
+    
+    def isReady(self):
+        return self.state == counter_state.idle
 
     def isRunning(self):
-        return self.Running == 1
+        return self.state == counter_state.running
+    
+    def isDone(self):
+        return self.state == counter_state.done
+
+    def reset(self):
+        self.state << counter_state.idle
+        self.Count << 0
+
 
 
     def InTimeWindowSLV(self,TimeMin=port_in(v_slv()),TimeMax=port_in(v_slv()), DataIn=port_in(v_slv())):
         
         DataOut=v_slv("DataIn'length")
         DataOut << 0
-        if self.Running and TimeMin <= self.Count and self.Count < TimeMax:
+        if self.isRunning() and TimeMin <= self.Count and self.Count < TimeMax:
             DataOut << DataIn
 
         return DataOut
@@ -54,7 +73,7 @@ class counter(v_class):
     def InTimeWindowSl(self,TimeMin=port_in(v_slv()),TimeMax=port_in(v_slv())):
         DataOut=v_sl()
         DataOut << 0
-        if self.Running and TimeMin <= self.Count and self.Count < TimeMax:
+        if self.isRunning() and TimeMin <= self.Count and self.Count < TimeMax:
             DataOut << 1  
         return DataOut
         
@@ -68,6 +87,7 @@ def main():
     sp = args.PackageName.split("_")
     ax = v_package(args.PackageName,sourceFile=__file__,
     PackageContent = [
+      v_enum ( counter_state),        
       counter(16)
     ]
     
